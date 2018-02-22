1e898bd7ec304b98a85b2eb2959f202f083e779a
taniaunid <36333822+taniaunid@users.noreply.github.com>
2018-02-22 15:30:53 -0600

    urlmodificada

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import web
 
-db_host = 'localhost'
+db_host = 'o3iyl77734b9n3tg.cbetxkdyhwsb.us-east-1.rds.amazonaws.com'
 db_name = 'tiendasp'
-db_user = 'taniaunid'
-db_pw = 'hidalgo'
+db_user = 'o3iyl77734b9n3tg.cbetxkdyhwsb.us-east-1.rds.amazonaws.com'
+db_pw = 'iplnypvz0xzh0vqv'
 
 db = web.database(
     dbn='mysql',
